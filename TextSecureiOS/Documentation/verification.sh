a673b1776982d3c6ad7b415dfb4b90f1dd81a986
Christine Corbett Moran <christine.corbett@gmail.com>
2013-09-21 01:42:11 +0200

    moved curl test doc to Documentation/verification.sh

--- TextSecureiOS/Documentation/verification.sh
+++ TextSecureiOS/Documentation/verification.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env sh
 #test server verification
-curl -X PUT -i -H "Content-Type:application/json" --data "{\"verificationCode\" : \"269536762\", \"authenticationToken\" :
-\"1234\" , \"gcmRegistrationId\" : \"12345\"}" https://textsecure-gcm.appspot.com/v1/accounts/+41799624499
+# Example:
+#REGISTER +41791111111:
+curl -k -X POST --header "Content-Length: 0" https://gcm.textsecure.whispersystems.org/v1/accounts/sms/+41791111111
+# AUTHORIZE:
+curl -k -X PUT -i -H "Authorization: Basic KzQxNzk5NjI0NDk5OjI1OTNlNWZhZTdiNzUxODYxYzcxOTE4YjRhNGU5YTE5" -H "Content-Type:application/json" --data "{\"signalingKey\" : \"Ti71M5PR63/SOnrermsyZMlrl2WrwAMD/5cH5Z/bjKEG1e3jjKzUBf1zI0bPt4ai\"}"  https://gcm.textsecure.whispersystems.org/v1/accounts/code/111111
+# with 111111 being the verification code the phone number recieved by SMS
+
+#NOTE: k means ignoring the SSL warning, what we should be doing rather is using Moxie's cert
