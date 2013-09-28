f44745db20965a08db5584d18c8c79b5933d8d94
Frederic Jacobs <github@fredericjacobs.com>
2013-09-28 20:47:06 +0200

    Revert "Revert "Trying to figure out how these new iOS7 foundation classes are working""
    This reverts commit 8bb8802a06cf403add5128c8420810099ce0722d.

---
+++ TextSecureiOS/Networking/Requests/TSRequest.h
@@ -0,0 +1,13 @@
+//
+//  TSRequest.h
+//  TextSecureiOS
+//
+//  Created by Frederic Jacobs on 9/27/13.
+//  Copyright (c) 2013 Open Whisper Systems. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TSRequest : NSURLRequest
+
+@end
