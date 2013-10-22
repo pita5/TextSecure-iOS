5e6429f97baf4f627b1aee123d6d162dfec0d8fc
Frederic Jacobs <github@fredericjacobs.com>
2013-10-22 20:04:52 +0200

    New provisioning

---
+++ TextSecureiOS/ViewControllers/ConversationViewController.h
@@ -0,0 +1,13 @@
+//
+//  ConversationViewController.h
+//  TextSecureiOS
+//
+//  Created by Frederic Jacobs on 10/21/13.
+//  Copyright (c) 2013 Open Whisper Systems. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ConversationViewController : UIViewController
+
+@end
