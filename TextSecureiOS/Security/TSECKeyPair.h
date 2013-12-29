bde8e46cfe27876be2fba7b187801d12a27ce027
Alban D <nabla.c0d3@gmail.com>
2013-12-29 10:42:55 -0800

    Add documentation

--- TextSecureiOS/Security/TSECKeyPair.h
+++ TextSecureiOS/Security/TSECKeyPair.h
@@ -15,9 +15,31 @@
 }
 
 
+/**
+ * Generate a Curve25519 key pair.
+ * @author Alban Diquet
+ *
+ * @param preKeyId The TextSecure preKeyId to assign to the newly generated key pair. See https://github.com/WhisperSystems/TextSecure/wiki/ProtocolV2 for more information.
+ * @return A reference to the newly generated key pair or nil if memory could not be allocated.
+ */
 +(instancetype) keyPairGenerateWithKeyId:(int32_t)preKeyId;
 
+
+/**
+ * Export the key pair's public key.
+ * @author Alban Diquet
+ *
+ * @return The key pair's public key.
+ */
 -(NSData*) getPublicKey;
+
+
+/**
+ * Compute a shared secret using the supplied third-party public key and the key pair's private key. See https://code.google.com/p/curve25519-donna/ for more information.
+ * @author Alban Diquet
+ *
+ * @return The shared secret.
+ */
 -(NSData*) generateSharedSecretFromPublicKey:(NSData*)theirPublicKey;
 
 
