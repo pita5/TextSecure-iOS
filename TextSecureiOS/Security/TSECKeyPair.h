710e972b7c299d6590ec6e40757c58e8da2a1306
Alban D <nabla.c0d3@gmail.com>
2013-12-29 10:07:11 -0800

    Revamp ECKeyPair pt1 MONEYMONEY

---
+++ TextSecureiOS/Security/TSECKeyPair.h
@@ -0,0 +1,27 @@
+//
+//  TSECKeyPair.h
+//  TextSecureiOS
+//
+//  Created by Alban Diquet on 12/29/13.
+//  Copyright (c) 2013 Open Whisper Systems. All rights reserved.
+//
+
+
+@interface TSECKeyPair : NSObject<NSCoding> {
+
+    uint8_t publicKey[32];
+    uint8_t privateKey[32];
+    int32_t preKeyId;
+}
+
+
++(instancetype) keyPairGenerateWithKeyId:(int32_t)preKeyId;
+
+-(NSData*) getPublicKey;
+-(NSData*) generateSharedSecretFromPublicKey:(NSData*)theirPublicKey;
+
+-(void) encodeWithCoder:(NSCoder*)coder;
+-(instancetype) initWithCoder:(NSCoder*)coder;
+
+
+@end
