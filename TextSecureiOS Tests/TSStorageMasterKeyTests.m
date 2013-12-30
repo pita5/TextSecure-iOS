62162c1779d82c461114b087caa8436493cdf962
Alban D <nabla.c0d3@gmail.com>
2013-12-29 20:43:39 -0800

    Remove TSEncryptedDatabase+Private.h

---
+++ TextSecureiOS Tests/TSStorageMasterKeyTests.m
@@ -0,0 +1,34 @@
+//
+//  TSStorageMasterKeyTests.m
+//  TextSecureiOS
+//
+//  Created by Alban Diquet on 12/29/13.
+//  Copyright (c) 2013 Open Whisper Systems. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TSStorageMasterKeyTests : XCTestCase
+
+@end
+
+@implementation TSStorageMasterKeyTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here; it will be run once, before the first test case.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here; it will be run once, after the last test case.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
