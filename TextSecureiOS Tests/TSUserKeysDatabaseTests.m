d25d2d49ce5a678b3ffc266e4761ea2561597fd1
Alban D <nabla.c0d3@gmail.com>
2013-12-29 19:39:35 -0800

    Add unit tests for TSUserKeysDatabase

---
+++ TextSecureiOS Tests/TSUserKeysDatabaseTests.m
@@ -0,0 +1,58 @@
+//
+//  TSUserKeysDatabaseTests.m
+//  TextSecureiOS
+//
+//  Created by Alban Diquet on 12/29/13.
+//  Copyright (c) 2013 Open Whisper Systems. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+#import "TSUserKeysDatabase.h"
+#import "TSStorageMasterKey.h"
+
+@interface TSUserKeysDatabaseTests : XCTestCase
+
+@end
+
+
+static NSString *storageKeyPw = @"1234test";
+
+
+@implementation TSUserKeysDatabaseTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Remove any existing DB
+    [TSUserKeysDatabase databaseErase];
+    
+    // Create a storage master key
+    [TSStorageMasterKey eraseStorageMasterKey];
+    [TSStorageMasterKey createStorageMasterKeyWithPassword:storageKeyPw];
+}
+
+- (void)tearDown
+{
+    // Put teardown code here; it will be run once, after the last test case.
+    [super tearDown];
+}
+
+
+- (void)testDatabaseCreate
+{
+    NSError *error = nil;
+    XCTAssertTrue([TSUserKeysDatabase databaseCreateUserKeysWithError:&error], @"database creation failed");
+    XCTAssertNil(error, @"database creation returned an error");
+    
+    
+    XCTAssertNotNil([TSUserKeysDatabase getAllPreKeys], @"database creation returned nil prekeys");
+    XCTAssertNotNil([TSUserKeysDatabase getIdentityKey], @"database creation returned nil identity key");
+    // Check for key of last resort
+    XCTAssertNotNil([TSUserKeysDatabase getPreKeyWithId:0xFFFFFF], @"database creation returned nil for key of last resort");
+}
+
+
+
+
+@end
