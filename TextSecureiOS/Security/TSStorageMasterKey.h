0d6265fdf9a7fd75859c6d05bac7ed5d10bad1e4
Alban D <nabla.c0d3@gmail.com>
2014-01-04 13:31:24 -0800

    Add documentation to TSStorageMasterKey

--- TextSecureiOS/Security/TSStorageMasterKey.h
+++ TextSecureiOS/Security/TSStorageMasterKey.h
@@ -11,15 +11,69 @@
 @interface TSStorageMasterKey : NSObject
 
 
+#pragma mark Storage master key generation
+/**
+ * Create a storage master key on the device which will be used to encrypted the TextSecure databases.
+ * @author Alban Diquet
+ *
+ * @param userPassword The password from which the master key should be derived.
+ * @param error.
+ * @return The newly generated storage master key or nil of an error occured.
+ */
 +(NSData*) createStorageMasterKeyWithPassword:(NSString *)userPassword error:(NSError **) error;
+
+/**
+ * Check if the storage master key has already been generated on the device.
+ * @author Alban Diquet
+ *
+ * @return TRUE if key has already been generated on the device.
+ */
 +(BOOL) wasStorageMasterKeyCreated;
 
+
+#pragma mark Storage master key access
+/**
+ * Unlock the storage master key.
+ * @author Alban Diquet
+ *
+ * @param userPassword The password from which the master key was derived on creation.
+ * @param error.
+ * @return The storage master key or nil of an error occured.
+ */
 +(NSData*) unlockStorageMasterKeyUsingPassword:(NSString *)userPassword error:(NSError **)error;
+
+/**
+ * Get the previously unlocked storage master key.
+ * @author Alban Diquet
+ *
+ * @param error.
+ * @return The storage master key or nil of an error occured (such as the master key being locked).
+ */
 +(NSData*) getStorageMasterKeyWithError:(NSError **)error;
 
+
+#pragma mark Storage master key locking
+/**
+ * Lock the storage master key, thereby requiring a call to unlockStorageMasterKeyUsingPassword:error before the key can be recovered.
+ * @author Alban Diquet
+ */
 +(void) lockStorageMasterKey;
+
+
+/**
+ * Check if the storage master key is currently in a "locked" state.
+ * @author Alban Diquet
+ *
+ * @return TRUE if key is locked.
+ */
 +(BOOL) isStorageMasterKeyLocked;
 
+
+#pragma mark Storage master key deletion
+/**
+ * Erase the storage master key from the device.
+ * @author Alban Diquet
+ */
 +(void) eraseStorageMasterKey;
 
 
