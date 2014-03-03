948a2742ec9b1de8a2c444388635c936f0c3acf4
Daniel Witurna <daniel.witurna@gmail.com>
2014-03-03 16:20:32 +0100

    Added tests (long string, utf8chars) for password unlock view.

--- TextSecureiOS Tests/PasswordUnlockViewControllerTests.m
+++ TextSecureiOS Tests/PasswordUnlockViewControllerTests.m
@@ -98,5 +98,39 @@ static NSString *masterPw = @"1234test";
     XCTAssertFalse(didUnlock, @"Password unlock with wrong password succeeded");
 }
 
+- (void)testVeryLongPassword
+{
+    UITextField *passwordTextField = self.controller.passwordTextField;
+    // Creating a string with NSUIntegerMax length will lead to out-of-memory situation.
+    // After trying various values, decided for 2^20, which is still pretty long and
+    // works on 32-bit Simulator in a reasonable amount of time.
+    NSUInteger length = pow(2,20);
+    NSString *veryLongString = [@"" stringByPaddingToLength:length withString:@"a" startingAtIndex:0];
+    passwordTextField.text = veryLongString;
+    
+    [self.controller textFieldShouldReturn:passwordTextField]; //Triggers unlock button press
+    
+    BOOL didUnlock = ![TSStorageMasterKey isStorageMasterKeyLocked];
+    
+    XCTAssertFalse(didUnlock, @"Password unlock with wrong password succeeded");
+}
+
+- (void)testUnusualCharactersPassword{
+    UITextField *passwordTextField = self.controller.passwordTextField;
+    // Code for creating string with a lot of different unicode characters found
+    // here http://cocoadev.com/UniCode
+    NSMutableString *testString = [[NSMutableString alloc] initWithCapacity:55296-32];
+    for (int i = 32; i < 55296; i++) {
+        [testString appendFormat:@"%C", (unichar)i];
+    }
+    passwordTextField.text = testString;
+    
+    [self.controller textFieldShouldReturn:passwordTextField]; //Triggers unlock button press
+    
+    BOOL didUnlock = ![TSStorageMasterKey isStorageMasterKeyLocked];
+    
+    XCTAssertFalse(didUnlock, @"Password unlock with wrong password succeeded");
+}
+
 
 @end
