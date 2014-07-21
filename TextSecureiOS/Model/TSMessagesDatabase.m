e73a94c8d54f177eca2df000927cce550f441f8f
Christine Corbett Moran <christine.corbett@gmail.com>
2014-07-21 13:35:29 -1000

    merged old group work

--- TextSecureiOS/Model/TSMessagesDatabase.m
+++ TextSecureiOS/Model/TSMessagesDatabase.m
@@ -464,6 +464,7 @@ static TSDatabaseManager *messagesDb = nil;
 + (NSArray*)groups{
     openDBMacroNil
     
+#warning TODO to finish this
     NSMutableArray *groups = [NSMutableArray array];
     
     [messagesDb.dbQueue inDatabase:^(FMDatabase *db) {
@@ -473,17 +474,17 @@ static TSDatabaseManager *messagesDb = nil;
             TSGroup *group = [[TSGroup alloc] init];
             TSAttachment *attachment = [[TSAttachment alloc] initWithAttachmentDataPath:[searchInDB stringForColumn:@"avatar_path"] withType:[searchInDB intForColumn:@"avatar_type"] withDecryptionKey:[searchInDB dataForColumn:@"avatar_key"]];
             group.groupImage = [UIImage imageWithData:[Cryptography decryptAttachment:[NSData dataWithContentsOfFile:attachment.attachmentDataPath] withKey:attachment.attachmentDecryptionKey]];
-            group.groupContext = [[TSGroupContext alloc] initWithId:[TSGroupContext getDecodedId:[searchInDB stringForColumn:@"group_id"]] withName:group.groupName withAvatar:attachment];
+//            group.groupContext = [[TSGroupContext alloc] initWithId:[TSGroupContext getDecodedId:[searchInDB stringForColumn:@"group_id"]] withName:group.groupName withAvatar:attachment];
             [groups addObject:group];
         }
         [searchInDB close];
     }];
     for (TSGroup* group in groups) {
         [messagesDb.dbQueue inDatabase:^(FMDatabase *db) {
-            FMResultSet *searchInDB = [db executeQuery:@"SELECT * FROM group_membership WHERE group_id=?" withArgumentsInArray:@[[group.groupContext getEncodedId]];
+            FMResultSet *searchInDB = [db executeQuery:@"SELECT * FROM group_membership WHERE group_id=?" withArgumentsInArray:@[[group.groupContext getEncodedId]]];
             while ([searchInDB next]) {
 #warning relay isn't stored, because we're missing this in the TSPushMessageContent... hmmmm
-                [group.groupContext.members addObject: [[TSContact alloc] initWithRegisteredID:[searchInDB stringForColumn:@""] relay:nil]];
+  //              [group.groupContext.members addObject: [[TSContact alloc] initWithRegisteredID:[searchInDB stringForColumn:@""] relay:nil]];
             
             
             }
